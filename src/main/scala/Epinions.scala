27281146a492e5a1c7ca58e965f454fe93d8fc82
Gci04 <gcinzoe04@gmail.com>
2019-04-11 16:15:48 +0300

    Add following methods: - embedding Matrix method - create batches Created Network object

--- src/main/scala/Epinions.scala
+++ src/main/scala/Epinions.scala
@@ -6,6 +6,8 @@ import org.apache.spark.mllib.recommendation.{ALS, Rating}
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types.{IntegerType, StructField, StructType}
 import breeze.linalg.{DenseMatrix, DenseVector}
+import breeze.numerics.log
+import org.apache.spark.mllib.random.UniformGenerator
 
 object Epinions {
   def main(args: Array[String]) {
@@ -19,10 +21,18 @@ object Epinions {
       .config("spark.master", "local")
       .getOrCreate()
 
+    val sprk_contenxt: SparkContext = spark.sparkContext
+
 //    read train and test data from program argument
 
     val path_train = args(0)
     val path_test = args(1)
+    // number of epochs
+    //val iterations = 10
+    val iterations = args(2).toInt
+    //learning rate
+    val alpha = args(3).toDouble
+
 
     val data = read_data(path_train, spark)
     val test = read_data(path_test, spark).repartition(10)
@@ -30,9 +40,33 @@ object Epinions {
     println("---successful read of data---")
 
     val batch_size = 500
-    val batches =  partition(data, batch_size)
+    val batches =  create_batches(data, batch_size)
 
     println("---successful partition of data---")
+
+
+    //create and initialize embedding matrices
+    val n_nodes = 40334
+    val embedding_size = 50
+    var incoming_embedding =embeddingMatrix(embedding_size,n_nodes)
+    var outgoing_embeding = embeddingMatrix(embedding_size,n_nodes)
+
+    var neg_samples = 20
+    //Start train
+    for (i <- 1 to iterations){
+      for (batch in batches){
+        var emb_in_broadcast = sprk_contenxt.broadcast(incoming_embedding)
+        var emb_out_broadcast = sprk_contenxt.broadcast(outgoing_embeding)
+
+        //calculate gradients
+        for (k <- in_grads_local.keys) {
+          /*
+           * Update weights in column k of emb_in
+           */
+        }
+      }
+      //update gradients just like gradient descent
+    }
 
   }
 
@@ -45,18 +79,45 @@ object Epinions {
       .load(path)
       .rdd.map(row => (row.getAs[Int](0), row.getAs[Int](1)))
   }
+  def embeddingMatrix(embeding_size: Int, vocab_len: Int ) = {
+    var generator = new UniformGenerator()
 
-  def partition(data: RDD[(Int, Int)], batch_size: Int) = {
+    var data = Array.fill(vocab_len * embeding_size)(generator.nextValue().toFloat)
+    new DenseMatrix(embeding_size, vocab_len, data)
+  }
+
+  def create_batches(data: RDD[(Int, Int)], batch_size: Int) = {
     " "
   }
 
+
   def estimate_gradients_for_edge(source: Int,destination: Int,emb_in: DenseMatrix[Float],emb_out: DenseMatrix[Float]) = {
+
 
     val in = emb_in(::, source)
     val out = emb_out(::, destination)
+
+    ((source, in_grads), (destination, out_grads))
   }
 
   def get_top(source: Int, top_k: Int) = {
 
   }
+}
+
+object Network{
+  def gradients(): Unit ={
+
+  }
+  def gradients_updates(): Unit ={
+
+  }
+
+  def sigma(x:Double): Double ={
+    1. / (1. + Math.exp(-x))
+  }
+  def loss (x:Double) = {
+    -log(sigma(x))
+  }
+
 }
