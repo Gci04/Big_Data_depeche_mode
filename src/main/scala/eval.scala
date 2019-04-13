0779eb5a72cc3e50b8008691ff90625d5ae1b7aa
132rr9e0 <kulaevmlds@gmail.com>
2019-04-13 18:26:58 +0300

    filtering out existing edges

--- src/main/scala/eval.scala
+++ src/main/scala/eval.scala
@@ -14,3 +14,32 @@ def sort_neighbours_by_rating (neighbors_ratings : Array[((Int,Int),Double)]) : 
 //   ((2,3),8.9))
 // var sorted_by_rating_neighbors = sort_neighbours_by_rating(neighbors_ratings)
 // println(sorted_by_rating_neighbors.deep.mkString("\n"))
+
+//------------------------------------------------------------------------------------------------------------------------
+// FILTER EXISTING EDGES
+// function for filtering processing_edges from already existed eedges
+def filter_existing_edges (existing_edges : Array[(Int,Int)], processing_edges_ratings : Array[((Int,Int),Double)]) : Array[((Int,Int),Double)] = {
+  // initializing of result
+  var result_array : Array[((Int,Int),Double)] = Array()
+  // iterating over processing_edges
+  for (edge_n_rating <- processing_edges_ratings) {
+    // check that edge did not exist early
+    if (!existing_edges.contains(edge_n_rating._1)) {
+      // append edge to result
+      result_array = result_array:+((edge_n_rating))
+    }
+  }
+
+   return (result_array)
+}
+// usage example
+// var existing_edges = Array((0,1),(0,2),(1,4),(2,3))
+// var processing_edges_ratings : Array[((Int,Int),Double)] = Array(
+//   ((0,1),12.6),
+//   ((1,2),0.0),
+//   ((1,3),-3.43),
+//   ((2,3),23.1),
+//   ((3,4),0.001))
+// var processing_edges_ratings_filtered = filter_existing_edges(
+//   existing_edges, processing_edges_ratings)
+// println(processing_edges_ratings_filtered.deep.mkString("\n"))
