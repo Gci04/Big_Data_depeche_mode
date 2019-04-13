83dc9055d8cebebe2e9668efdaddcd35ac91cd1f
132rr9e0 <kulaevmlds@gmail.com>
2019-04-13 21:19:55 +0300

    compact representation of neighbors

--- src/main/scala/eval.scala
+++ src/main/scala/eval.scala
@@ -48,31 +48,23 @@ def filter_existing_edges (existing_edges : Array[(Int,Int)], processing_edges_r
 // GET TOP 10 RATED NEIGHBORS
 // function for extracted 10 recomandations for each node
 def get_top_ten_neighbors (edges_ratings : Array[((Int,Int),Double)]) : Array[((Int,Int),Double)] = {
-  // initializing of the result
   var top_ten_overall : Array[((Int,Int),Double)] = Array()
-  // concat all sourceand destination edges together
-  var set_of_all_nodes = edges_ratings.map(_._1._1) ++ edges_ratings.map(_._1._2)
-  // iterating over set of unique nodes
-  for (node <- set_of_all_nodes.distinct) {
+  // iterating over set of nodes
+  var set_of_nodes = edges_ratings.map(_._1._1) ++ edges_ratings.map(_._1._2)
+  for (node <- set_of_nodes.distinct) {
     // println(node)
-    // initializing top 10 for node
     var top_ten_for_node : Array[((Int,Int),Double)] = Array()
-    // initializing array of all edges for node
     var edges_ratings_for_node : Array[((Int,Int),Double)] = Array()
     // iterating over edges and ratings pairs
     for (edge_n_rating <- edges_ratings) {
-      // check that node is present in the edge
       if (edge_n_rating._1._1==node || edge_n_rating._1._2==node) {
         // println(edge_n_rating)
-        // add such edge for the node
         edges_ratings_for_node = edges_ratings_for_node:+((edge_n_rating))
       }
     }
-    // select top 10 for a node
     top_ten_for_node = sort_neighbours_by_rating(edges_ratings_for_node).take(10)
     // println("Top 10:")
     // println(top_ten_for_node.deep.mkString("\n"))
-    // update result with node's top 10
     top_ten_overall = top_ten_overall ++ top_ten_for_node
   }
   return (top_ten_overall.distinct)
@@ -94,6 +86,45 @@ def get_top_ten_neighbors (edges_ratings : Array[((Int,Int),Double)]) : Array[((
 //   ((3,6),4.0))
 // var top_ten_overall = get_top_ten_neighbors(edges_rating)
 // println(top_ten_overall.deep.mkString("\n")) 
+
+//------------------------------------------------------------------------------------------------------------------------
+// TOP TEN INTO COMPACT FORMAT
+// function for representation nodestop rated neighbors in compact format
+def top_ten_to_nodes_representation (top_ten_overall : Array[((Int,Int),Double)]) : Array[(Int, Array[Int])] = {
+  // initializing result top ten for all nodes representation
+  var top_ten_in_nodes_representation : Array[(Int, Array[Int])] = Array()
+  // concatenate all nodes
+  var set_of_nodes = top_ten_overall.map(_._1._1) ++ top_ten_overall.map(_._1._2)
+  // iterating over set of unique nodes
+  for (node <- set_of_nodes.distinct) {
+    // initializing top ten neighbors for selected node
+    var node_top_ten_neighbors : Array[Int] = Array()
+    // concat neighbors into one array
+    var int_null : Int = null.asInstanceOf[Int]
+    node_top_ten_neighbors ++= top_ten_overall.map(x => if (x._1._1==node) x._1._2 else int_null).filterNot(_==int_null)
+    node_top_ten_neighbors ++= top_ten_overall.map(x => if (x._1._2==node) x._1._1 else int_null).filterNot(_==int_null)
+    // update resulting array with selected node's neighbors
+    top_ten_in_nodes_representation :+= (node, node_top_ten_neighbors)
+  }
+
+  return (top_ten_in_nodes_representation)
+}
+// var edges_rating = Array(
+//   ((0,1),12.6),
+//   ((1,2),-7.0),
+//   ((1,3),6.0),
+//   ((1,4),9.0),
+//   ((1,5),7.0),
+//   ((1,6),5.0),
+//   ((1,7),3.0),
+//   ((1,8),3.0),
+//   ((1,9),2.0),
+//   ((1,10),1.0),
+//   ((1,11),0.0),
+//   ((9,8),0.0),
+//   ((3,6),4.0))
+// var top_ten_in_nodes_representation = top_ten_to_nodes_representation(edges_rating)
+// println(top_ten_in_nodes_representation.map(x=>(x._1,'['+x._2.mkString(",")+']')).mkString("\n"))
 
 //------------------------------------------------------------------------------------------------------------------------
 // PERFORMING DEFINED STEPS IN A SEQUENCE
