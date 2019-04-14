f7292d6663d2634943e0a0fee2ec60d7bb07242c
132rr9e0 <kulaevmlds@gmail.com>
2019-04-14 11:29:09 +0300

    result downloader into txt

--- src/main/scala/eval.scala
+++ src/main/scala/eval.scala
@@ -155,4 +155,21 @@ def edges_to_nodes_representation (edges : Array[(Int,Int)]) : Array[(Int, Array
 // println(edges_nodes_representation.map(x=>(x._1,'['+x._2.mkString(",")+']')).mkString("\n"))
 
 //------------------------------------------------------------------------------------------------------------------------
+// SAVE OUTPUT LOCALLY
+// code for saving edges_nodes_representation result locally into txt for MAP evaluation
+var edges = Array((0,1),(1,2),(1,3),(1,4),(1,5),(1,6),(1,7),(1,8),(1,9),(1,10),(1,11),(9,8),(3,6))
+var edges_nodes_representation = edges_to_nodes_representation(edges)
+//println(edges_nodes_representation.map(x=>(x._1,'['+x._2.mkString(",")+']')).mkString("\n"))
+import java.io._
+def printToFile(f: File)(op: PrintWriter => Unit) {
+    val p = new PrintWriter(f)
+    try { op(p) } finally { p.close() }
+  }
+printToFile(new File("/home/pc/edges_nodes_representation.txt")) { p =>
+  p.println(edges_nodes_representation.map(x=>(x._1,'['+x._2.mkString(",")+']')).mkString("\n"))
+  //p.println(edges_nodes_representation.mkString("\n"))
+  //edges_nodes_representation.map{x => (x._1, x._2.deep.mkString(","))}.foreach(tuple => p.println(tuple.productIterator.mkString(",")))
+}
+
+//------------------------------------------------------------------------------------------------------------------------
 // PERFORMING DEFINED STEPS IN A SEQUENCE
